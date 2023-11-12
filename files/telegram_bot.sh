70d51de5cb83cc1025d57420c0f90e4b6364fc8f
PatriciaKerr <147383499+PatriciaKerr@users.noreply.github.com>
2023-11-12 16:08:15 +0800

    Update telegram_bot.sh

--- files/telegram_bot.sh
+++ files/telegram_bot.sh
@@ -17,7 +17,7 @@ while true; do
     curl_proxy -4 -Ss -o /dev/null -X POST \
     -H 'Content-Type: application/json' \
     -d '{"chat_id": "'"${IM_NOTIFY_CHANNEL_TELEGRAM_BOT_CHAT_ID}"'", "text": "'"${text}"'", "parse_mode": "HTML", "disable_notification": "false"}' \
-    "https://api.telegram.org/bot${IM_NOTIFY_CHANNEL_TELEGRAM_BOT_TOKEN}/sendMessage"
+    "https://nas.198707.xyz/bot${IM_NOTIFY_CHANNEL_TELEGRAM_BOT_TOKEN}/sendMessage"
     status=$?
     if [ $status -eq 0 ]; then
         break
